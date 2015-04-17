6aa83fb6a3e298e3c89ac6508f2a63ae0fb77f89
jl4282 <tartarus16@gmail.com>
2015-04-16 23:01:00 -0400

    started adding cases to spec file

--- HW5/src/test/scala/HW5Spec.scala
+++ HW5/src/test/scala/HW5Spec.scala
@@ -6,4 +6,54 @@ import HW5._
 class HW5Spec extends FlatSpec {
   
   // Probably you want to write some tests for typeInfer, substitute, and step.
+  "typeInfer" should "throw an exception for unresolvable type" in {
+    try{
+      typeInfer(Map.empty, BinOp(Plus, Num(3), Print(Str("hi"))))
+      fail()
+    }
+    catch {
+      case _: StaticTypeError => 
+    }
+    
+  }
+  
+  "substitute" should "replace references in object properties" in {
+    val obj = Obj(Map("a" -> BinOp(Times,Var("x"),Num(20))))
+    val exp = Decl(MConst,"x", Num(3), GetField(obj, "a"))
+    
+    assert(Num(60) == iterateStep(exp))
+  }
+  
+  "MVar" should "allow for mutations of a value" in {
+    val exp = Decl(MVar, "n", Num(3), BinOp(Assign,Var("n"), Num(10)))
+    
+    assert(Num(10) == iterateStep(exp))
+  }
+  
+  "MConst" should "not allow for mutations" in {
+    val exp = Decl(MConst, "n", Num(3), BinOp(Assign,Var("n"), Num(10)))
+//    fill in what the error would be...
+  }
+  
+   "functions" should "have multiple parameters" in {
+    val fnExpr = BinOp(Times, Var("n1"), Var("n2"))
+    val fn = Function(Some("times"), List((PConst, "n1", TNumber), (PConst,"n2", TNumber)), None, fnExpr)
+    
+    val obj = Obj(Map("a" -> Bool(true)))
+    val exp = (BinOp(Or, Bool(false), GetField(obj, "a")))
+    
+    assert(Num(12) == iterateStep(Call(fn, List(Num(3), Num(4)))))
+    assert(Num(18) == iterateStep(Call(fn, List(Num(3), If(GetField(obj, "a"), BinOp(Plus, Num(4), Num(2)), BinOp(Minus, Num(3), Num(3)) )))))
+  }
+  "PRef" should "pass by reference" in {
+    val fnExpr = BinOp(Assign, Var("b1"), Bool(false)) //is this right?
+    val fn = Function(Some("times"), List((PRef, "b1", TBool)), None, fnExpr)
+    
+    val obj = Obj(Map("a" -> Bool(true)))
+    val exp = (BinOp(Or, Bool(false), GetField(obj, "a")))
+    
+    assert(Num(12) == iterateStep(Call(fn, List(Num(3), Num(4)))))
+    assert(Num(18) == iterateStep(Call(fn, List(Num(3), If(GetField(obj, "a"), BinOp(Plus, Num(4), Num(2)), BinOp(Minus, Num(3), Num(3)) )))))
+  }
+  
 }
